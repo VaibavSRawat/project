3dd15e9889a21fe1910c12e1cc5d77594c6815ab
VaibavSRawat <38232695+VaibavSRawat@users.noreply.github.com>
2018-04-14 23:04:32 +0530

    Update pro.c

--- pro.c
+++ pro.c
@@ -1,27 +1,46 @@
 #include<stdio.h>
 #include<pthread.h>
+#include<time.h>
+#include<stdlib.h>
 #include<unistd.h>
 #include<semaphore.h>
 sem_t mutex,empty,full;
-static int buff=10;
+static int buff=0;
 void *pro()
-{
-sem_wait(&empty);
-sem_wait(&mutex);
-buff++;
-printf("Producer produce item and buffer size now is %d",buff);
-sem_post(&mutex);
-sem_post(&full);
-}
+	{
+	while(1)
+		{
+		sem_wait(&empty);
+		sem_wait(&mutex);
+		if(buff<10)
+			{
+			buff++;
+			printf("\nProducer produce item and buffer size now is %d",buff);
+			}
+		else if(buff>=10)
+		printf("\nBuffer is already full no space left to produce");
+		sem_post(&mutex);
+		sem_post(&full);
+		sleep(1 + rand()%2);
+		}
+	}
 void *con()
-{
-sem_wait(&full);
-sem_wait(&mutex);
-buff--;
-printf("Consumer consumed item remaining item %d",buff);
-sem_post(&mutex);
-sem_post(&empty);
-
+	{
+	while(1)
+	{
+		sem_wait(&full);
+		sem_wait(&mutex);
+		if(buff!=0)
+			{
+			buff--;
+			printf("\nConsumer consumed item remaining item %d",buff);
+			}
+		else if(buff==0)
+			printf("\nbuffer is empty nothing to consume");
+		sem_post(&mutex);
+		sem_post(&empty);
+		sleep(1 + rand()%6);
+	}
 }
 void main()
 {
@@ -29,6 +48,11 @@ pthread_t p1,c2;
 sem_init(&mutex,2,1);
 sem_init(&empty,2,10);
 sem_init(&full,2,0);
-pthread_create(&p1,NULL,pro,NULL);
-pthread_create(&c2,NULL,con,NULL);
+for(int i=0;;)
+	{
+	pthread_create(&p1,NULL,pro,NULL);
+	pthread_create(&c2,NULL,con,NULL);
+	pthread_join(p1,NULL);
+	pthread_join(c2,NULL);
+	}
 }
