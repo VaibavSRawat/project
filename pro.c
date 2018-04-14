25baeb679e412574e51aa52467cc53d783878be7
VaibavSRawat <38232695+VaibavSRawat@users.noreply.github.com>
2018-04-14 22:09:22 +0530

    Update pro.c

--- pro.c
+++ pro.c
@@ -1,63 +1,34 @@
-#include<unistd.h>
 #include<stdio.h>
-#include<stdlib.h>
 #include<pthread.h>
-static int buff;
-void *producer();
-void *consumer();
-int main()
+#include<unistd.h>
+#include<semaphore.h>
+sem_t mutex,empty,full;
+static int buff=10;
+void *pro()
 {
-int a;
-pthread_mutex_t m1;
-char ch;
-pthread_mutex_init(&m1,NULL);
-pthread_t pro,con;
-do
-{
-printf("\nEnter one of the following");
-printf("\nEnter 1 for producer");
-printf("\nEnter 2 for consumer");
-scanf("%d",&a);
-if(a==1)
-{
-pthread_create(&pro,NULL,producer,NULL);
-  pthread_join(pro,NULL);
-}
-else if(a==2)
-{
-pthread_create(&con,NULL,consumer,NULL);
-  
- 
-pthread_join(con,NULL);
-}
-else
-{
-printf("\nwrong choice try again");
-
-printf("\nPress y to enter more");
-scanf("%c",&ch);
-}
-}
-while(ch=='y'|ch=='Y');
-
-}
-void *producer()
-{
-pthread_mutex_lock(&m1);
-//sleep(1);
-printf("\nlock aquired by the producer and now producing");
+sem_wait(&empty);
+sem_wait(&mutex);
 buff++;
-printf("Producer produced item and buffer size is: %d",buff);
-printf("\n now releasing lock");
-pthread_mutex_unlock(&m1);
+printf("Producer produce item and buffer size now is %d",buff);
+sem_post(&mutex);
+sem_post(&full);
 }
-void *consumer()
+void *con()
 {
-pthread_mutex_lock(&m1);
-//sleep(1);
-printf("\nlock aquired by the consumer and now consuming");
+sem_wait(&full);
+sem_wait(&mutex);
 buff--;
-printf("Consumer consumed an item and remainng buffer size is: %d",buff);
-printf("\n now releasing lock");
-pthread_mutex_unlock(&m1);
+printf("Consumer consumed item remaining item %d",buff);
+sem_post(&mutex);
+sem_post(&empty);
+
+}
+void main()
+{
+pthread_t p1,c2;
+sem_init(&mutex,2,1);
+sem_init(&empty,2,10);
+sem_init(&full,2,0);
+pthread_create(&p1,NULL,pro,NULL);
+pthread_create(&c2,NULL,con,NULL);
 }
